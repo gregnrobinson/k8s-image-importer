b8f918395442ba7407c522ca574bca5df0ca8bd3
Greg Robinson <gregnrobinson@gmail.com>
2022-02-01 22:11:01 -0500

    add support for manifests using urls

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ export bold=$(tput bold)
 export normal=$(tput sgr0)
 
 registry_prefix="docker.io/gregnrobinson"
-target_dir=""
+target_dir="./online-boutique"
 target_url="https://github.com/jetstack/cert-manager/releases/download/v1.7.0/cert-manager.yaml"
 
 if [[ ! -z $target_url ]]
 then
-  wget -q $target_url -O manifest.yaml
-  grep -n "image: " manifest.yaml | awk -F  ": " '{print $3}' | sed -e 's/^"//' -e 's/"$//' > base_urls
+  wget -q $target_url -O manifest.yaml &&\
+  grep -n "image: " manifest.yaml | awk -F  ": " '{print $3}' | sed -e 's/^"//' -e 's/"$//' > target_url
   cat base_urls | while read line
   do
       name=$(echo $line | cut -f3-4 -d"/")
@@ -29,7 +29,7 @@ fi
 if [[ ! -z $target_dir ]]
 then
   pushd "$target_dir"
-  grep -n "image: " manifest.yaml | awk -F  ": " '{print $3}' | sed -e 's/^"//' -e 's/"$//' > base_urls
+  grep -n "image: " *.yaml | awk -F  ": " '{print $3}' | sed -e 's/^"//' -e 's/"$//' > target_dir
 
   cat base_urls | while read line
   do  
